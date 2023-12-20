f683a141afac98370231d28914a7358e3ecd9e6b
Jeremiah Williams <williamsjeremiahkenneth@gmail.com>
2023-12-20 09:36:33 -0500

    added google map to UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Map from './components/map.js';
 
 function App() {
   return (
     <div>
-      
+      <Map />
     </div>
   );
 }
---
+++ client/src/components/map.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '50vw',
+  height: '50vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+const center = {
+  lat: 40.440624, // default latitude
+  lng: -79.995888, // default longitude
+};
+
+const Map = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyDh61Lq5p9k3IAdKWbiNPtRS-qWUkn2bRE',
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default Map;
